d82faa3d5c86fbe2ee7a5dd183be3b68eef59c27
Jess <jessicacostaesilva97@gmail.com>
2023-07-28 20:36:07 -0300

    updated download and upload data to use multithreads

--- etl/app.py
+++ etl/app.py
@@ -1,8 +1,8 @@
-# from extract_data_azure import DataDownloader
 from .send_data_s3 import DataUploader
 from .extract_data_azure import DataDownloader
 import datetime
 import logging
+from threading import Thread
 
 # Configuração do logging
 logging.basicConfig(
@@ -13,15 +13,33 @@ logging.basicConfig(
 uploader = DataUploader()
 downloader = DataDownloader()
 
+today = datetime.date.today().strftime('%Y-%m-%d')
+
+MAX_THREADS = 40
+
 def upload_data_s3():
-    today = datetime.date.today().strftime('%Y-%m-%d')
+    threads_list = list()
     for file_name in downloader.get_file_names():
         if file_name[len(file_name) - 3:] == '.gz':
-            file_name_split = file_name.split('/')
-            s3_file_name = f'source-files/{file_name_split[0]}/ingestion_date={today}/{file_name_split[2]}'
-            logging.info(file_name)
-            logging.info(s3_file_name)
-            uploader.upload_file(downloader.download_blob(file_name), s3_file_name)
+            thread = Thread(target=lambda: download_and_upload_file(file_name=file_name))
+            threads_list.append(thread)
+            thread.start()
+            logging.info(f'Threads count:{len(threads_list)}')
+            if(len(threads_list) == MAX_THREADS):
+                for thread in threads_list:
+                    thread.join()
+                threads_list = list()
+
+def download_and_upload_file(file_name):
+    logging.info(f'Starting new thread for download file: {file_name}')
+    socoped_downloader = DataDownloader()
+    uploader.upload_file(socoped_downloader.download_blob(file_name), get_s3_file_name(file_name))
+
+def get_s3_file_name(file_name):
+    file_name_split = file_name.split('/')
+    return f'source-files/{file_name_split[0]}/ingestion_date={today}/{file_name_split[2]}'
+    
+
 
 # source-files/amazon-reviews/ingestion_date="date"/file
 # source-files/amazon-metadata/ingestion_date="date"/file
