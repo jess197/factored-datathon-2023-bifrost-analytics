4103ef929f3302983cb1206ddf1610822e346380
Jess <jessicacostaesilva97@gmail.com>
2023-08-01 09:45:17 -0300

    update dag which updates bronze silver and gold layers with the streaming data

--- dags/dag_dbt_update.py
+++ dags/dag_dbt_update.py
@@ -3,6 +3,12 @@ from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 import os 
 from datetime import datetime 
 
+default_args = {
+    "owner": "Bifrost Analytics - Jess",
+    "retries": 1,
+    "retry_delay": 0 
+}
+
 profile_config = ProfileConfig(
     profile_name="bifrost-analytics",
     target_name="dev",
@@ -21,10 +27,12 @@ dag_dbt_update = DbtDag(
         dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
     ),
     # normal dag parameters
-    schedule_interval="0 19-23,0-2 * * *",
+    schedule_interval="0 22-23,0-5 * * *",
     start_date=datetime(2023, 7 , 31),
+    default_args=default_args,
     catchup=False,
     dag_id="dag_dbt_update",
+    
 )
 
 
