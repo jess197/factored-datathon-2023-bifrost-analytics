bb87f52fda353a8cfc66745532a7e58db4428a9a
Luan José de Almeida Cardoso <luan.jose.c@gmail.com>
2023-08-02 01:02:45 -0300

    Modelagem incremental para a tabela de streaming

--- astro/dags/dbt/bifrost_analytics/models/silver/reviews.sql
+++ astro/dags/dbt/bifrost_analytics/models/silver/reviews.sql
@@ -4,38 +4,6 @@
 with bronze_amz_reviews as
 (
 
-  select reviews_key
-       , asin 
-       , overall 
-       , review_text
-       , reviewer_id
-       , reviewer_name
-       , summary
-       , review_time 
-       , verified
-       , ingestion_date
-    FROM {{ ref('amazon_reviews') }}
-
-), 
-
-bronze_amz_reviews_streaming as (
-
-    select str_reviews_key 
-       , asin 
-       , overall 
-       , review_text
-       , reviewer_id
-       , reviewer_name
-       , summary
-       , review_time 
-       , verified
-       , ingestion_date
-    FROM {{ ref('amazon_reviews_streaming') }}
-
-),
-
-silver_amz_reviews(
-
  SELECT distinct reviews_key as surr_key_reviews
       , asin::varchar(100) as asin
       , overall::varchar(5) as overall
@@ -46,8 +14,12 @@ silver_amz_reviews(
       , DATE(TO_TIMESTAMP_NTZ(review_time)) as review_time
       , verified::boolean as verified
       , ingestion_date 
-   FROM bronze_amz_reviews
-  UNION 
+    FROM {{ ref('amazon_reviews') }}
+
+), 
+
+bronze_amz_reviews_streaming as (
+
  SELECT distinct str_reviews_key as surr_key_reviews
       , asin::varchar(100) as asin
       , overall::varchar(5) as overall
@@ -58,7 +30,23 @@ silver_amz_reviews(
       , DATE(TO_TIMESTAMP_NTZ(review_time)) as review_time
       , verified::boolean as verified
       , ingestion_date 
+    FROM {{ ref('amazon_reviews_streaming') }}
+
+),
+
+silver_amz_reviews(
+
+   SELECT 
+      *
+   FROM bronze_amz_reviews
+   WHERE
+      review_date >= (SELECT MAX(review_date) FROM AMZ_DATA_SILVER.REVIEWS )
+  UNION 
+   SELECT
+      *
    FROM bronze_amz_reviews_streaming
+   WHERE
+      review_date >= (SELECT MAX(review_date) FROM AMZ_DATA_SILVER.REVIEWS )
 
 )
 
