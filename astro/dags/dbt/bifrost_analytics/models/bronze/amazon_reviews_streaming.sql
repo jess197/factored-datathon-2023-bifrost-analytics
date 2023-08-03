dda52cebc795672b32ab7b212715546641ec0a58
Jess <jessicacostaesilva97@gmail.com>
2023-08-03 17:11:53 -0300

    updates in reviews

--- astro/dags/dbt/bifrost_analytics/models/bronze/amazon_reviews_streaming.sql
+++ astro/dags/dbt/bifrost_analytics/models/bronze/amazon_reviews_streaming.sql
@@ -3,29 +3,29 @@
 
 {{ config(materialized='table') }}
 
-with source_amz_reviews_streaming as (
+with source_amz_reviews_streaming AS (
 
     SELECT PARSE_JSON(rf.value), rs.last_modified_date
     FROM AMAZON.RAW.REVIEWS_STREAMING rs,
     LATERAL FLATTEN(input => rs.RAW_FILE) rf
 ),
 
-amz_reviews_streaming as (
+amz_reviews_streaming AS (
 
-    SELECT $1:asin as asin 
-        , $1:internal_partition as internal_partition
-        , $1:overall as overall
-        , $1:partition_number as partition_number 
-        , $1:reviewText as review_text
-        , $1:reviewerID as reviewer_id
-        , $1:reviewerName as reviewer_name
-        , $1:style as style 
-        , $1:summary as summary
-        , $1:unixReviewTime as review_time
-        , $1:verified as verified
-        , $1:vote as vote
-        , last_modified_date as ingestion_date 
-        , {{ dbt_utils.generate_surrogate_key(['partition_number','asin']) }} as str_reviews_key
+    SELECT $1:asin AS asin 
+        , $1:internal_partition AS internal_partition
+        , $1:overall AS overall
+        , $1:partition_number AS partition_number 
+        , $1:reviewText AS review_text
+        , $1:reviewerID AS reviewer_id
+        , $1:reviewerName AS reviewer_name
+        , $1:style AS style 
+        , $1:summary AS summary
+        , $1:unixReviewTime AS review_time
+        , $1:verified AS verified
+        , $1:vote AS vote
+        , last_modified_date AS ingestion_date 
+        , {{ dbt_utils.generate_surrogate_key(['partition_number','asin']) }} AS str_reviews_key
     FROM source_amz_reviews_streaming
 
 )
